59a81789da7ac15dd2f4f1f3421a14871fd41f8d
Natete-Verite <nateteverite@gmail.com>
2022-07-25 15:17:12 +0300

    Added the splash screen to the MHM App

---
+++ app/src/main/java/dev/verite/mhm/TeaserOne.kt
@@ -0,0 +1,11 @@
+package dev.verite.mhm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TeaserOne : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_teaser_one)
+    }
+}
